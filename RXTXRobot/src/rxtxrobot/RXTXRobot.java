9457f0c039526039997a4d8c2f4a7cda01c9983e
Christopher King <ccking@smu.edu>
2011-08-30 19:14:10 -0500

    Added more methods to address additional API functions

--- RXTXRobot/src/rxtxrobot/RXTXRobot.java
+++ RXTXRobot/src/rxtxrobot/RXTXRobot.java
@@ -9,6 +9,7 @@ public class RXTXRobot
     private OutputStream out;
     private InputStream in;
     private byte[] buffer;
+    private String lastResponse;
     public RXTXRobot(String p)
     {
         port = p;
@@ -85,13 +86,25 @@ public class RXTXRobot
         debug("Sending command: " + s);
         try
         {
-            if (out != null)
+            if (out != null || in != null)
+            {
                 out.write((s+"\r\n").getBytes());
+                if (verbose)
+                {
+                    sleep(2000);
+                    in.read(buffer, 0, Math.min(in.available(), buffer.length));
+                    lastResponse = new String(buffer);
+                }
+            }
         }
         catch(IOException e)
         {
             System.err.println("Cannot write command (IOException)! Error: " + e.getMessage());
         }
+    }
+    public String getLastResponse()
+    {
+        return lastResponse;
     }
     public void sleep(int length)
     {
@@ -104,7 +117,7 @@ public class RXTXRobot
             System.err.println("Thread was interrupted (InterruptedException). Error: " + e.getMessage());
         }
     }
-    public String readAnalogPins()
+    public String getAnalogPins()
     {
         try
         {
@@ -121,7 +134,7 @@ public class RXTXRobot
         }
         return "";
     }
-    public String readDigitalPins()
+    public String getDigitalPins()
     {
         try
         {
@@ -137,5 +150,51 @@ public class RXTXRobot
             System.err.println("Cannot read command (IOException)! Error: " + e.getMessage());
         }
         return "";
+    }
+    public void setAnalogPin(int pin, int value)
+    {
+        debug("Setting Analog Pin " + pin + " to " + value);
+        sendRaw("w a " + pin + " " + value);
+    }
+    public void setDigitalPin(int pin, int value)
+    {
+        debug("Setting Digital Pin " + pin + " to " + value);
+        sendRaw("w d " + pin + " " + value);
+    }
+    public void moveServo(int servo, int position)
+    {
+        debug("Moving servo " + servo + " to position " + position);
+        if (position <= 0 || position >= 180)
+            System.err.println("ERROR: moveServo position must be greater than 0 and less than 180.  You supplied " + position + ", which is invalid.");
+        else
+            sendRaw("v " + servo + " " + position);
+    }
+    public void moveBothServos(int pos1, int pos2)
+    {
+        debug("Moving both servos to positions " + pos1 + " and " + pos2);
+        if (pos1 <= 0 || pos1 >= 180 || pos2 <= 0 || pos2 >= 180)
+            System.err.println("ERROR: moveBothServos positions must be greater than 0 and less than 180.  You supplied " + pos1 + " and " + pos2 + ".  One or more are invalid.");
+        else
+            sendRaw("V " + pos1 + " " + pos2);
+    }
+    public void moveStepper(int stepper, int steps)
+    {
+        debug("Moving stepper " + stepper + " " + steps + " steps");
+        sendRaw("p " + stepper + " " + steps);
+    }
+    public void moveStepper(int stepper1, int steps1, int stepper2, int steps2)
+    {
+        debug("Moving steppers " + stepper1 + " and " + stepper2 + " to positions " + steps1 + " and " + steps2);
+        sendRaw("P " + stepper1 + " " + steps1 + " " + stepper2 + " " + steps2);
+    }
+    public void runMotor(int motor, int speed, int time)
+    {
+        debug("Running motor " + motor + " at speed " + speed + " for time of " + time);
+        sendRaw("d " + motor + " "  + speed + " " + time);
+    }
+    public void runMotor(int motor1, int speed1, int motor2, int speed2, int time)
+    {
+        debug("Running two motors, motor " + motor1 + " at speed " + speed1 + " and motor " + motor2 + " at speed " + speed2 + " for time of " + time);
+        sendRaw("D " + motor1 + " " + speed1 +" " + motor2 + " " + speed2 + " " + time);
     }
 }
