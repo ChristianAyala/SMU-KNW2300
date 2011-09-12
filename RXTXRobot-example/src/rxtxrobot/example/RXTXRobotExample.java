f88b24cd5670571d0a81203c6bb7e447def9a66f
root <root@localhost>
2011-09-12 17:33:34 -0500

    A small implementation of RXTXRobot to move straight (both wheels) forward for 1 second, pause for 2 seconds, and move straight (both wheels) backwards for 1 second.

--- RXTXRobot-example/src/rxtxrobot/example/RXTXRobotExample.java
+++ RXTXRobot-example/src/rxtxrobot/example/RXTXRobotExample.java
@@ -5,6 +5,8 @@
 package rxtxrobot.example;
 
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import rxtxrobot.RXTXRobot;
 
 public class RXTXRobotExample {
@@ -12,7 +14,7 @@ public class RXTXRobotExample {
     public static void main (String [] args)
     {
         RXTXRobot r = new RXTXRobot("/dev/ttyUSB0",true);
-        Scanner s = new Scanner(System.in);
+        /*Scanner s = new Scanner(System.in);
         String t = "";
         do
         {
@@ -21,9 +23,16 @@ public class RXTXRobotExample {
             if (!t.equals("exit"))
             {
                 r.sendRaw(t);
+                System.out.println("Response -->"+r.getLastResponse());
             }
         }
-        while(!t.equals("exit"));
+        while(!t.equals("exit"));*/
+        System.out.println("Moving forward...");
+        r.runMotor(RXTXRobot.MOTOR1,2000,RXTXRobot.MOTOR2,2000,1000);
+        r.sleep(3000);
+        System.out.append("Moving backwards....");
+        r.runMotor(RXTXRobot.MOTOR1,-2000,RXTXRobot.MOTOR2,-2000,1000);
+        r.sleep(2000);
         System.out.println("Closing...");
         r.close();
         System.out.println("Done");
