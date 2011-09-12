ff665f48c57ff15c3a4845949b7b63d432f5a5d8
root <root@localhost>
2011-09-12 17:32:25 -0500

    Added comments and a list of public/private methods at the intro comments

--- RXTXRobot/src/rxtxrobot/RXTXRobot.java
+++ RXTXRobot/src/rxtxrobot/RXTXRobot.java
@@ -1,3 +1,30 @@
+/*  RXTXRobot API package
+ *   
+ *  All the methods in this package are asyncronous.  Therefore, if you move a motor for a certain amount
+ *  of time, be sure to either Thread.sleep (or use the RXTXRobot.sleep method) or account for the execution
+ *  time in your implementation
+ * 
+ * 
+ *   public:
+ *     +RXTXRobot(String port, [boolean verbose, [int bufferSize]])
+ *     +close()
+ *     +sendRaw(String str)
+ *     +getLastResponse()
+ *     +sleep(int length)
+ *     +getAnalogPins()
+ *     +getDigitalPins()
+ *     +setAnalogPin(int pin, int value)
+ *     +setDigitalPin(int pin, int value)
+ *     +moveServo(int servo, int position)
+ *     +moveBothServos(int position_1, int position_2)
+ *     +moveStepper(int stepper_1, int steps_1, [int stepper_2, int steps_2])
+ *     +runMotor(int motor_1, int speed_1, [int motor_2, int speed_2], int time)
+ * 
+ *   private:
+ *     -init()
+ *     -debug(String str)
+ */
+
 package rxtxrobot;
 import gnu.io.*;
 import java.io.*;
@@ -178,6 +205,11 @@ public class RXTXRobot
         debug("Setting Digital Pin " + pin + " to " + value);
         sendRaw("w d " + pin + " " + value);
     }
+    /* moveServo(int servo,int position)
+     *  * Moves the specified servo to the specified position
+     *   - servo should be either RXTXRobot.SERVO1 or RXTXRobot.SERVO2
+     *   - position should be a number between 0 and 180 (not inclusive)
+     */
     public void moveServo(int servo, int position)
     {
         if (servo != RXTXRobot.SERVO1 && servo != RXTXRobot.SERVO2)
@@ -191,6 +223,11 @@ public class RXTXRobot
         else
             sendRaw("v " + servo + " " + position);
     }
+    /* moveBothServos(int position1, int position2)
+     *  * Moves both servos simultaneously to their supplied positions
+     *   - position1 should be a number between 0 and 180 (not inclusive) for RXTXRobot.SERVO1 to move to
+     *   - position2 should be a number between 0 and 180 (not inclusive) for RXTxRobot.SERVO2 to move to
+     */
     public void moveBothServos(int pos1, int pos2)
     {
         debug("Moving both servos to positions " + pos1 + " and " + pos2);
@@ -209,6 +246,12 @@ public class RXTXRobot
         debug("Moving steppers " + stepper1 + " and " + stepper2 + " to positions " + steps1 + " and " + steps2);
         sendRaw("P " + stepper1 + " " + steps1 + " " + stepper2 + " " + steps2);
     }
+    /* runMotor(int motor, int speed, int time)
+     *  * Runs the specified motor the specified speed for the specified time (milliseconds)
+     *   - motor should be either RXTXRobot.MOTOR1 or RXTXRobot.MOTOR2
+     *   - speed should be an integer.  Negative number means reverse, positive number means forward
+     *   - time should be an integer representing milliseconds, 0 means indefinitely until defined
+     */
     public void runMotor(int motor, int speed, int time)
     {
         if (motor != RXTXRobot.MOTOR1 && motor != RXTXRobot.MOTOR2)
@@ -219,6 +262,14 @@ public class RXTXRobot
         debug("Running motor " + motor + " at speed " + speed + " for time of " + time);
         sendRaw("d " + motor + " "  + speed + " " + time);
     }
+    /* runMotor(int motor1, int speed1, int motor2, int speed2, int time)
+     *  * Runs two specified motors simultaneously at the specified speeds for the specified time (milliseconds)
+     *   - motor1 should be either RXTXRobot.MOTOR1 or RXTXRobot.MOTOR2
+     *   - speed1 should be an integer.  Negative number means reverse, positive number means forward
+     *   - motor2 should be either RXTXRobot.MOTOR1 or RXTXRobot.MOTOR2
+     *   - speed2 should be an integer.  Negative number means reverse, positive number means forward
+     *   - time should be an integer representing milliseconds, 0 means indefinitely until defined
+     */
     public void runMotor(int motor1, int speed1, int motor2, int speed2, int time)
     {
         if ((motor1 != RXTXRobot.MOTOR1 && motor1 != RXTXRobot.MOTOR2) || (motor2 != RXTXRobot.MOTOR1 && motor2 != RXTXRobot.MOTOR2))
