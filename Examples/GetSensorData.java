3800225f9998e34e85efee1c543ca9a7e24b0581
Chris King <ccking@smu.edu>
2012-09-18 16:50:09 -0500

    Fixed sensor data example

--- Examples/GetSensorData.java
+++ Examples/GetSensorData.java
@@ -7,24 +7,14 @@ public class GetSensorData
         // All sensor data will be read from the analog pins
 	
         RXTXRobot r = new RXTXRobot("COM5"); //Create object on COM5
+	
+	r.refreshAnalogPins();
 
-        //Create a temporary integer array to capture the sensor data
-        int[] temp = r.getAnalogPins();
-
-        // Read the data from each array position and print out the value.
-        // Array positions will be 0-5 corresponding to the analog pin numbers
-        for(int x = 0; x < temp.length; x++)
+	for (int x=0; x < RXTXRobot.NUM_ANALOG_PINS; ++x)
 	{
-            System.out.println("Sensor " + (x+1) + " value: " + temp[x]);
-        }
-        //you can also access each singular position in the array. 
-        System.out.println("Sensor 1 value: " + temp[0]);
-        System.out.println("Sensor 2 value: " + temp[1]);
-        System.out.println("Sensor 3 value: " + temp[2]);
-        System.out.println("Sensor 4 value: " + temp[3]);
-        System.out.println("Sensor 5 value: " + temp[4]);
-        System.out.println("Sensor 6 value: " + temp[5]);
-        
-        r.close();
+		AnalogPin temp = r.getAnalogPin(x);
+		System.out.println("Sensor " + x + " has value: " + temp.getValue());
+	}
+	r.close();
     }
 }
