f7dd55f7bd37d6a99278684c4de506ab75eca063
Chris King <ccking@smu.edu>
2012-09-26 19:18:49 -0500

    Revert "Updated the example that is used to show how to read/write to the sensors"
    This reverts commit 679fda70efc5ea6950dc527d9ff9a0b4fe171a2a.

--- Examples/GetSensorData.java
+++ Examples/GetSensorData.java
@@ -8,23 +8,13 @@ public class GetSensorData
 	
         RXTXRobot r = new RXTXRobot("COM5"); //Create object on COM5
 	
-	r.refreshAnalogInputPins();
+	r.refreshAnalogPins();
 
 	for (int x=0; x < RXTXRobot.NUM_ANALOG_PINS; ++x)
 	{
-		AnalogInputPin temp = r.getAnalogInputPin(x);
+		AnalogPin temp = r.getAnalogPin(x);
 		System.out.println("Sensor " + x + " has value: " + temp.getValue());
 	}
-
-	// Write to an Analog Pin (turn ON), there is also an OFF constant
-	AnalogOutputPin p = r.getAnalogOutputPin(3);
-	p.setValue(AnalogOutputPin.ON);
-	
-	// Read a digital pin
-	r.refreshDigitalPins();
-	DigitalPin d = r.getDigitalPin(2);
-	System.out.println("Digital pin 2 has value: " + d.getValue());
-
 	r.close();
     }
 }
