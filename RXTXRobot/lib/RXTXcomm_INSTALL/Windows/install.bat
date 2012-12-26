056a85e7ff529f897d54fd53430778d5a0a70b27
Chris King <ccking@smu.edu>
2012-12-26 17:15:48 -0600

    Updated formatting of Windows install batch file.  Probably will be changed to an MSI soon

--- RXTXRobot/lib/RXTXcomm_INSTALL/Windows/install.bat
+++ RXTXRobot/lib/RXTXcomm_INSTALL/Windows/install.bat
@@ -1,11 +1,11 @@
 @ECHO OFF
 set javafound=n
 set save=%cd%
-ECHO "======================================="
-ECHO "|   RXTXRobot Installer for Windows   |"
-ECHO "======================================="
+ECHO ^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=
+ECHO ^|   RXTXRobot Installer for Windows   ^|
+ECHO ^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=
 
-ECHO "Installing Java Libraries......................................"
+ECHO Installing Java Libraries......................................
 IF NOT exist %save%\libs\32-bit\rxtxSerial.dll goto fatalerror
 IF NOT exist %save%\libs\64-bit\rxtxSerial.dll goto fatalerror
 IF exist "C:\Program Files (x86)\" goto checkOldBits
@@ -18,7 +18,7 @@ goto checkRegular
 
 :doOldBits
 CD "C:\Program Files (x86)\Java\"
-FOR /D /r %%i IN (bin*) ( COPY  %save%\libs\32-bit\rxtxSerial.dll %%i )
+FOR /D /r %%i IN (bin*) ( COPY  %save%\libs\32-bit\rxtxSerial.dll %%i >nul )
 set javafound=y
 goto checkRegular
 
@@ -29,7 +29,7 @@ goto closing
 
 :doRegular
 CD "C:\Program Files\Java\"
-FOR /D /r %%i IN (bin*) ( COPY %save%\libs\64-bit\rxtxSerial.dll %%i )
+FOR /D /r %%i IN (bin*) ( COPY %save%\libs\64-bit\rxtxSerial.dll %%i >nul )
 set javafound=y
 goto closing
 
@@ -39,26 +39,35 @@ goto closing
 
 :doRegularNo64
 CD "C:\Program Files\Java\"
-FOR /D /r %%i IN (bin*) ( COPY %save%\libs\32-bit\rxtxSerial.dll %%i )
+FOR /D /r %%i IN (bin*) ( COPY %save%\libs\32-bit\rxtxSerial.dll %%i >nul )
 set javafound=y
 goto closing
 
 
 
 :fatalerror
-ECHO "FATAL ERROR:"
-ECHO "The required libraries were not found!  You must have:"
-ECHO "   %save%\libs\32-bit\rxtxSerial.dll"
-ECHO "   %save%\libs\64-bit\rxtxSerial.dll"
+ECHO FATAL ERROR:
+ECHO The required libraries were not found!  You must have:
+ECHO    %save%\libs\32-bit\rxtxSerial.dll
+ECHO    %save%\libs\64-bit\rxtxSerial.dll
 goto end
 
 :closing
-IF %javafound%==y ( ECHO "ERROR: No Java installation was found" ) ELSE ( ECHO "Java Installation has completed SUCCESSFULLY" )
+IF %javafound%==y (
+	ECHO        ERROR: No Java installation was found 
+) ELSE (
+	ECHO        Java Installation has completed SUCCESSFULLY 
+)
 goto end
 
 :end
-ECHO "Installing Arduino UNO Drivers (R2 and R3)........................."
+ECHO.
+ECHO Installing Arduino UNO Drivers (R2 and R3).........................
 %SystemRoot%\System32\InfDefaultInstall.exe %save%\libs\Drivers\Arduino_UNO.inf
-ECHO "Driver Installation has completed SUCCESSFULLY"
-ECHO "Installation Ended."
+ECHO         Driver Installation has completed SUCCESSFULLY
+ECHO.
+ECHO.
+ECHO Installation Ended.
+CD %save%
+PAUSE
 
