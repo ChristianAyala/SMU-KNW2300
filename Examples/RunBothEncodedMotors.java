e1a323776760acb283797a7ecc9684a8b31f4eb6
Chris King <ccking@smu.edu>
2013-03-26 15:04:15 -0500

    Fixed example code

--- Examples/RunBothEncodedMotors.java
+++ Examples/RunBothEncodedMotors.java
@@ -7,7 +7,7 @@ public class RunBothEncodedMotors
 	{
 		RXTXRobot r = new RXTXRobot(); // Create RXTXRobot object
 		r.setPort("COM2"); // Set port to COM2
-		r.setHasEncodedMotor(true);
+		r.setHasEncodedMotors(true);
 		r.connect();
 		r.runEncodedMotor(RXTXRobot.MOTOR1, 255, 100000, RXTXRobot.MOTOR2, 255, 50000); // Run both motors forward, one for 100,000 ticks and one for 50,000 ticks.
 		r.close();
--- Examples/RunEncodedMotor.java
+++ Examples/RunEncodedMotor.java
@@ -7,7 +7,7 @@ public class RunEncodedMotor
 	{
 		RXTXRobot r = new RXTXRobot(); // Create RXTXRobot object
 		r.setPort("COM3"); // Set port to COM3
-		r.setHasEncodedMotor(true);
+		r.setHasEncodedMotors(true);
 		r.connect();
 		r.runEncodedMotor(RXTXRobot.MOTOR1, 255, 100000); // Run motor 1 forward (speed of 255) for 100,000 ticks
 		// Program stops until the command above is completed
