2d06f6086482fc43a4919b4d3cc764685370dcfd
Christian Ayala <cayala@smu.edu>
2014-03-20 19:56:02 -0500

    Added version checking thread, a bunch of comments, and Globals class for global things

---
+++ RXTXRobot/src/rxtxrobot/Global.java
@@ -0,0 +1,83 @@
+package rxtxrobot;
+
+import java.io.PrintStream;
+
+/**
+ * Class with common functionality and "global" data accessible to the entire
+ * API.
+ *
+ */
+public class Global {
+        /**
+         * Global major version.
+         * 
+         * Update this for significant API/firmware rewrites. If this does
+         * not match the major version of the firmware, the communication with
+         * the Arduino will likely not work.
+         */
+        final protected static int VERSION_MAJOR = 3;
+        
+        /**
+         * Global minor version.
+         * 
+         * Update this for regular API updates that required a corresponding
+         * firmware change to the Arduino. If this is ahead of the minor version
+         * of the firmware, then new expected functionality will likely not work.
+         * If this is behind the minor version of the firmware, then new hardware
+         * functionality will not be accessible from this API.
+         */
+        final protected static int VERSION_MINOR = 5;
+        
+        /**
+         * Global subminor version.
+         * 
+         * Update this for bug fixes and minor changes that did not
+         * require updates to the firmware. It should have no effect on the
+         * functionality of the API if this does not match the firmware subminor
+         * version.
+         */
+        final protected static int VERSION_SUBMINOR = 0;
+        private static PrintStream out_stream;
+        private static PrintStream err_stream;
+        
+        public static void setOutStream(PrintStream o)
+        {
+                out_stream = o;
+        }
+        
+        public static PrintStream getOutStream()
+        {
+                return out_stream;
+        }
+        
+        public static void setErrStream(PrintStream e)
+        {
+                err_stream = e;
+        }
+        
+        public static PrintStream getErrStream()
+        {
+                return err_stream;
+        }
+        
+        public static String getVersion()
+        {
+                return "" + VERSION_MAJOR + "." + VERSION_MINOR + "." + VERSION_SUBMINOR;
+        }
+        
+        public static void debug(String str)
+        {
+                getOutStream().println("--> " + str);
+        }
+        
+        public static void error(String str, String class_name, String method, boolean fatal)
+        {
+                if (fatal)
+                        getErrStream().println("FATAL ERROR (method: " + method + "):");
+                else
+                        getErrStream().println("ERROR (method: " + method + " ): ");
+                getErrStream().println("Message:");
+                str = str.replaceAll("\\n", "\n\t");
+                getErrStream().println("\t" + str);
+        }
+}
---
+++ RXTXRobot/src/rxtxrobot/VersionCheckThread.java
@@ -0,0 +1,69 @@
+package rxtxrobot;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * 
+ * Thread class to check the most recent version of the API by using the FYD site
+ * @author Chris Ayala
+ */
+public class VersionCheckThread extends Thread 
+{
+        private static final int TIMEOUT = 2*1000;
+        private static final String VERSION_URL = "http://lyle.smu.edu/fyd/query.php?action=version";
+    
+        @Override
+        public void run()
+        {
+                try
+                {
+                        //Open the connection
+                        URLConnection connection = new URL(VERSION_URL).openConnection();
+                        connection.setReadTimeout(TIMEOUT);
+                        connection.setConnectTimeout(TIMEOUT);
+                        
+                        //Read the response
+                        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                        String inputLine;
+                        StringBuilder builder = new StringBuilder();
+                        while ((inputLine = in.readLine()) != null)
+                        {
+                                builder.append(inputLine);
+                        }
+                        String onlineVersion = builder.toString();
+                        
+                        //Do the comparison
+                        if (!Global.getVersion().equals(onlineVersion))
+                        {
+                                Global.error("There is an updated version of this API (version " + onlineVersion + ")" +
+                                                   "\nat lyle.smu.edu/fyd/downloads.php", "VersionCheck", "run", false);
+                        }
+                        else
+                        {
+                                Global.debug("You are on the most up-to-date version of the API! (version " + onlineVersion + ")");
+                        }
+                }
+                catch (MalformedURLException e) 
+                {
+                        Global.debug("VersionCheck - Malformed URL Exception: " + VERSION_URL);
+                }
+                catch (SocketTimeoutException e)
+                {
+                        Global.debug("VersionCheck - Socket timeout after " + TIMEOUT + " ms, failed to connect to the version check page");
+                }
+                catch (IOException e)
+                {
+                        Global.debug("VersionCheck - IOException - Could not connect to the version check server");
+                }
+                catch (Exception e)
+                {
+                        Global.debug("VersionCheck - General Exception");
+                }
+        }
+}
