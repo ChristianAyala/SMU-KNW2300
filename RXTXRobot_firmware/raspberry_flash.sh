c2a2a26d623c7e6c077beb87c4a3125446cab608
ChristianAyala <cayala@smu.edu>
2014-11-25 20:42:53 -0600

    Do a pull before flashing, to make sure its up to date

--- RXTXRobot_firmware/raspberry_flash.sh
+++ RXTXRobot_firmware/raspberry_flash.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+git pull origin master
 /usr/share/arduino/hardware/tools/avrdude -C/usr/share/arduino/hardware/tools/avrdude.conf -patmega328p -carduino -P/dev/ttyACM0 -b115200 -D -Uflash:w:/home/pi/SMU-KNW2300/RXTXRobot_firmware/RXTXRobot_firmware.cpp.hex:i
 
