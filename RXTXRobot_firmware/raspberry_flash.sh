3a5f9feef463df5d0bf58896d732c6921e144f9e
ChristianAyala <cayala@smu.edu>
2014-11-25 20:35:56 -0600

    Making notes of raspberry flashing stuff

---
+++ RXTXRobot_firmware/raspberry_flash.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+/usr/share/arduino/hardware/tools/avrdude -C/usr/share/arduino/hardware/tools/avrdude.conf -patmega328p -carduino -P/dev/ttyACM0 -b115200 -D -Uflash:w:/home/pi/SMU-KNW2300/RXTXRobot_firmware/RXTXRobot_firmware.cpp.hex:i
+
