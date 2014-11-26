1999611764f43021c6dbb0fc6cf89267779a8507
ChristianAyala <cayala@smu.edu>
2014-11-25 21:00:56 -0600

    Making the script work from anywhere

--- RXTXRobot_firmware/Raspberry_Flasher/raspberry_flash.sh
+++ RXTXRobot_firmware/Raspberry_Flasher/raspberry_flash.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+BASEDIR=$(dirname $0)
 git pull origin master
-/usr/share/arduino/hardware/tools/avrdude -C/usr/share/arduino/hardware/tools/avrdude.conf -patmega328p -carduino -P/dev/ttyACM0 -b115200 -D -Uflash:w:/home/pi/SMU-KNW2300/RXTXRobot_firmware/RXTXRobot_firmware.cpp.hex:i
+/usr/share/arduino/hardware/tools/avrdude -C/usr/share/arduino/hardware/tools/avrdude.conf -patmega328p -carduino -P/dev/ttyACM0 -b115200 -D -Uflash:w:$BASEDIR/RXTXRobot_firmware.cpp.hex:i
 
--- RXTXRobot_firmware/Raspberry_Flasher/raspberry_setup.sh
+++ RXTXRobot_firmware/Raspberry_Flasher/raspberry_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-sudo apt-get update && sudo apt-get install arduino arduino-core
-sudo ./alamode-setup/setup
+BASEDIR=$(dirname $0)
+sudo apt-get update && sudo apt-get install arduino arduino-core screen
+sudo $BASEDIR/alamode-setup/setup
 sudo reboot
 
